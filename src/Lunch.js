63663cc77db70abde8e03c1a94a806981da419ae
mashafi <mashafi.tech@gmail.com>
2021-01-21 00:20:45 +0500

    Update Lunch.js

--- src/Lunch.js
+++ src/Lunch.js
@@ -4,8 +4,8 @@ import React from 'react';
 function Lunch(props){
     return (
         <div>
-            <h1>Today We are Serving {props.dishName} in Lunch!!</h1>
-           <h1> {props.sweet} Will also be served!</h1>
+            <h1>Today We are Serving {props.dishName} in Lunch!</h1>
+           <h1> {props.sweet} Will also be served!!</h1>
         </div>
     )
 }

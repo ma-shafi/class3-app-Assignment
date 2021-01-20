135936f57e390893f1235e3925968406cb05ad30
mashafi <mashafi.tech@gmail.com>
2021-01-20 23:44:00 +0500

    added class3-app repo

---
+++ src/Lunch.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Lunch(props){
+    return (
+        <div>
+            <h1>Today We are Serving {props.dishName} in Lunch</h1>
+           <h1> {props.sweet} Will also be served</h1>
+        </div>
+    )
+}
+
+
+export default Lunch;
